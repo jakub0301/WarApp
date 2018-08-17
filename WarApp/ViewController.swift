268bbd7c721f060ec65058b7b6a70ebdcda60f29
jakub0301 <jakub0301sw@gmail.com>
2018-08-17 15:42:07 +0200

    Initial Commit

---
+++ WarApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WarApp
+//
+//  Created by Jakub0301 on 17/08/2018.
+//  Copyright © 2018 Jakub0301. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
